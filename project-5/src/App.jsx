e0cae1b7f78490e607c1bb94aa75556ecc2b3b15
Avadhoot Bhogil <58785159+Avadhoot5@users.noreply.github.com>
2024-07-01 23:06:52 +0530

    Project 5 initiated

---
+++ project-5/src/App.jsx
@@ -0,0 +1,17 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <p className="mt-5 pd-10 justify-start flex ">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugiat
+          repellendus odit accusamus iste quisquam sequi id vitae. Doloribus
+          porro facilis odio? Quasi, sint!
+        </p>
+      </div>
+    </>
+  );
+}
+
+export default App;
